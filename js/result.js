af0aff4b004b3dda27af73c09ef90f51544585f1
willowcheng <e.wingus@Gmail.com>
2015-09-26 15:55:47 -0400

    Add search result page

---
+++ js/result.js
@@ -0,0 +1,39 @@
+/**
+ * Created by willowcheng on 15-09-26.
+ */
+
+$(document)
+    .ready(function () {
+
+        // fix menu when passed
+        $('.masthead')
+            .visibility({
+                once: false,
+                onBottomPassed: function () {
+                    $('.fixed.menu').transition('fade in');
+                },
+                onBottomPassedReverse: function () {
+                    $('.fixed.menu').transition('fade out');
+                }
+            })
+        ;
+
+        // create sidebar and attach to menu open
+        $('.ui.sidebar')
+            .sidebar('attach events', '.toc.item')
+        ;
+        $('.ui.dropdown')
+            .dropdown({
+                on: 'click'
+            })
+        ;
+        $('.ui.radio.checkbox')
+            .checkbox()
+        ;
+        $('.special.cards .image').dimmer({
+            on: 'hover'
+        });
+
+
+    })
+;
