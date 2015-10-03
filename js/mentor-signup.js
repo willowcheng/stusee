b6f277f5ad1c90ef488a5ce4b88a7d64167c1717
willowcheng <e.wingus@Gmail.com>
2015-10-03 01:36:25 -0400

    Update signup link in navigation

---
+++ js/mentor-signup.js
@@ -0,0 +1,47 @@
+/**
+ * Created by willowcheng on 2015-10-03.
+ */
+
+$(document)
+    .ready(function () {
+        $('.ui.form')
+            .form({
+                fields: {
+                    email: {
+                        identifier: 'email',
+                        rules: [
+                            {
+                                type: 'empty',
+                                prompt: 'Please enter your e-mail'
+                            },
+                            {
+                                type: 'email',
+                                prompt: 'Please enter a valid e-mail'
+                            }
+                        ]
+                    },
+                    password: {
+                        identifier: 'password',
+                        rules: [
+                            {
+                                type: 'empty',
+                                prompt: 'Please enter your password'
+                            },
+                            {
+                                type: 'length[6]',
+                                prompt: 'Your password must be at least 6 characters'
+                            }
+                        ]
+                    }
+                }
+            })
+        ;
+
+        // create dropdown for some labels
+        $('.ui.dropdown')
+            .dropdown({
+                on: 'click'
+            })
+        ;
+    })
+;
